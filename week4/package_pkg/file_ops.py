ecd8d18eea3c50bb28bbb44c88b364a2aecd171f
Opeyemi Odejimi <opeyemiodejimi384@gmail.com>
2025-08-28 16:00:19 +0100

    Which kind task be this

---
+++ week4/package_pkg/file_ops.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+def save_participant(path, participant):
+    name
+
+# def load_participant(path):
---
+++ week4/test.py
@@ -0,0 +1,7 @@
+# Collect participants details
+print("-----Participant's Details----")
+participant_name = input("Enter your name: ")
+participant_age = input("Enter your age: ")
+participant_number = input("Enter your mobile number: ")
+participant_track = input("Enter your track: ")
+print(f"The name of the participant is {participant_name} and he is {participant_age} years old.")
